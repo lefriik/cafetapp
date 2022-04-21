598dd1fe6a542d16f30a17b9eaefc49312acd32e
admin <admin@MacBook-Pro-de-admin.local>
2022-04-21 15:38:48 -0400

    deploy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c1e4cac5763a41174a63e01cd9b574c5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '84819d00-8534-46f5-aa2b-52154aa77c4e'
+};
